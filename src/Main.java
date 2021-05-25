18357cb03ac0498d8f1b98a22e24e26ca10faa53
zakariya <34689684+99zakariya@users.noreply.github.com>
2021-05-25 10:09:47 +0530

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-    	System.out.println("Enter event name");
+    	System.out.println("Enter event name edit ");
     	String name=sc.nextLine();
         System.out.println("Enter the cost per day");
         double costPerDay=sc.nextDouble();
